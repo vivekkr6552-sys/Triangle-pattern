fb53e03cc46e2111a50ee8d744217afbcd7ddf19
Vivek Kumar <vivekkr6552@gmail.com>
2025-10-13 18:07:46 +0530

    Add files via upload

---
+++ printc.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main(){
+    int n;
+    cin>>n;                                                
+    for(int i=1; i<=n; i++){                                
+        for(int j=1; j<=n; j++){  
+            if(i==1 || i==n || j==1 ){
+                cout<<"C";
+
+            }
+        else{
+            cout<<" ";
+        }   
+                   
+
+        }
+         cout<<endl;   
+    }
+}
